0dfe6ca0831a49ed04bc2aa24ed60df4fbfbe8af
smae1993 <smae1993@gmail.com>
2022-09-18 15:59:16 +0430

    Initialize

--- lib/fatima_config.dart
+++ lib/fatima_config.dart
@@ -1,7 +1,9 @@
 import 'package:fatima/buttons/fatima_button_config.dart';
 import 'package:fatima/cards/fatima_card_config.dart';
+import 'package:fatima/text_fields/fatima_text_field_config.dart';
 
 class FatimaConfig {
   FButtonConfig buttonConfig = FButtonConfig();
   FatimaCardConfig cardConfig = FatimaCardConfig();
+  FatimaTextFieldConfig textFieldConfig = FatimaTextFieldConfig();
 }
