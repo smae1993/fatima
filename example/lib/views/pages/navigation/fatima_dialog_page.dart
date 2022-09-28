90fb9e27b16693545140f3bc0d144850095cfefd
smae1993 <smae1993@gmail.com>
2022-09-28 15:26:36 +0330

    Initialize

--- example/lib/views/pages/navigation/fatima_dialog_page.dart
+++ example/lib/views/pages/navigation/fatima_dialog_page.dart
@@ -1,5 +1,6 @@
 import 'package:example/views/layouts/app_layout.dart';
 import 'package:example/views/widgets/markdown_view.dart';
+import 'package:fatima/fatima.dart';
 import 'package:flutter/material.dart';
 
 class FatimaDialogPage extends StatelessWidget {
@@ -7,7 +8,31 @@ class FatimaDialogPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const AppLayout(
-        selected: "Dialog", body: MarkdownView("overview_en.md"));
+    return AppLayout(
+        selected: "Dialog",
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                // height: 500,
+                child: const MarkdownView("overview_en.md"),
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Fatima.dialog(SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Container(color: Colors.amberAccent),
+                        ));
+                      },
+                      child: Text("SimpleDialog"))
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
